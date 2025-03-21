28cea4c2318acfb7b4bd8cbf3eb92a4731dabc78
rbjunsu <jundol22@rainbow-robotics.com>
2025-03-21 10:28:01 +0900

    feat(about-template): new code

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stream>
+
+    template <typename T>
+    void add(T a, T b){
+        if(a>b) return a;
+            else b;
+    }
+
+
+int main(){
+    int a = 1, b=2;
+    std::cout << "result= " << add(1,2) << std::endl;
+}
